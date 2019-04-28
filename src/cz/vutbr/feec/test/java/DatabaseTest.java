4a360e131a68747c408e81be766f2dc0a292c849
cromize <mikezemanek@gmail.com>
2019-04-28 23:55:45 +0200

    add test for addJob()

--- src/cz/vutbr/feec/test/java/DatabaseTest.java
+++ src/cz/vutbr/feec/test/java/DatabaseTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import cz.vutbr.feec.core.Database;
 import cz.vutbr.feec.model.empl.AEmployee;
 import cz.vutbr.feec.model.empl.Assistant;
+import cz.vutbr.feec.model.empl.Dev;
+import cz.vutbr.feec.model.job.AssistJob;
 
 public class DatabaseTest {
   Database db = new Database();
@@ -27,6 +29,17 @@ public class DatabaseTest {
     assertTrue(db.getEmployees().size() == 1);
     db.removeEmployee(1);
     assertTrue(db.getEmployees().size() == 0);
+  }
+  
+  @Test
+  public void testAddJob() throws InstantiationException {
+    AEmployee empl1 = new Assistant(1, "name", "surname");
+    AEmployee empl2 = new Dev(2, "name", "surname");
+    db.addEmployee(empl1);
+    db.addEmployee(empl2);
+    assertTrue(db.getEmployee(1).getWorkHours() == 0);
+    db.addJob(new AssistJob());
+    assertTrue(db.getEmployee(1).getWorkHours() == 1);
   }
   
 }
